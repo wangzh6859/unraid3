4a8c02ff54981cbcd9ac045daa79c109c97eff1f
Zhihao Wang <62734787+wangzh6859@users.noreply.github.com>
2026-03-26 12:51:56 +0800

    Add search_text.py

---
+++ search_text.py
@@ -0,0 +1,11 @@
+import os
+import glob
+
+for root, dirs, files in os.walk('lib'):
+    for file in files:
+        if file.endswith('.dart'):
+            path = os.path.join(root, file)
+            with open(path, 'r', encoding='utf-8') as f:
+                content = f.read()
+                if "开发中" in content:
+                    print(f"FOUND in {path}")
