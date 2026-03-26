084ae9ea1764967a210a716df7262795a936ee1e
Zhihao Wang <62734787+wangzh6859@users.noreply.github.com>
2026-03-26 12:42:07 +0800

    Add fix_pubspec_version212.py

---
+++ fix_pubspec_version212.py
@@ -0,0 +1,12 @@
+import re
+with open('pubspec.yaml', 'r', encoding='utf-8') as f:
+    yaml = f.read()
+yaml = re.sub(r"version: .*", "version: 2.1.2+42", yaml)
+with open('pubspec.yaml', 'w', encoding='utf-8') as f:
+    f.write(yaml)
+
+with open('lib/main.dart', 'r', encoding='utf-8') as f:
+    code = f.read()
+code = re.sub(r"v\d+\.\d+\.\d+", "v2.1.2", code)
+with open('lib/main.dart', 'w', encoding='utf-8') as f:
+    f.write(code)
